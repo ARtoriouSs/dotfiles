6b05049020302f4ff2a207204b0dca3bedc8e323
Alexander Teslovskiy <artoriousso@gmail.com>
2019-09-19 19:04:44 +0300

    Fix global gitignore

--- scripts/create_symlinks.sh
+++ scripts/create_symlinks.sh
@@ -8,7 +8,6 @@ ln -sf $HOME/dotfiles/shell/.profile $HOME/.profile
 ln -sf $HOME/dotfiles/.gemrc $HOME/.gemrc
 ln -sf $HOME/dotfiles/.pryrc $HOME/.pryrc
 ln -sf $HOME/dotfiles/.gitconfig $HOME/.gitconfig
-ln -sf $HOME/dotfiles/.gitignore_global $HOME/.gitignore_global
 
 if [ ! -d "$HOME/.bundle" ]; then
   mkdir $HOME/.bundle
