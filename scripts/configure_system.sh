44447345b14da411e98bfc6b313a8672f8ca76f3
Alexander Teslovskiy <artoriousso@gmail.com>
2019-09-24 13:21:17 +0300

    Add setting for repeat delay

--- scripts/configure_system.sh
+++ scripts/configure_system.sh
@@ -4,5 +4,6 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
     # none
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     touch ~/.hushlogin # do not display login message
-    defaults write NSGlobalDomain KeyRepeat -int 0 # increase cursor repeat speed
+    defaults write -g KeyRepeat -int 0 # increase cursor repeat speed when holding arrow key (minimum in settings 2)
+    defaults write -g InitialKeyRepeat -int 10 # decreace delay before repeat starts (minimum in settings 15)
 fi
