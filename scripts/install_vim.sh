46cf8a2f20e59c22f2cb780d9716b1c75341f37c
Alexander Teslovskiy <artoriousso@gmail.com>
2019-12-09 17:32:54 +0300

    Fix vim installation

--- scripts/install_vim.sh
+++ scripts/install_vim.sh
@@ -9,12 +9,20 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
     brew install neovim
 fi
 
+# support tools
+gem install neovim
+rbenv rehash
+npm install -g neovim
+python -m pip install --user --upgrade pynvim
+python3 -m pip install --user --upgrade pynvim
+
 # symlink for config
 ln -sf ~/dotfiles/vim/.vimrc ~/.config/nvim/init.vim
 
 # setup color scheme
+SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
 mkdir $HOME/.config/nvim/colors
-cp ../vim/monokai.vim $HOME/.config/nvim/colors/
+cp $SCRIPT_PATH/vim/monokai.vim $HOME/.config/nvim/colors/
 
 # install Plug and plugins
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
