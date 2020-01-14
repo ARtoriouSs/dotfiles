a5a5cd039fa03405a0947070be1afcb81c2dff1e
Alexander Teslovskiy <artoriousso@gmail.com>
2020-01-14 19:27:39 +0300

    Add autotest.sh

---
+++ test/autotest.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# this script will run docker container with an empty system and then prepare_system.sh in it
+
+if [ "$1" = "--osx" ] || [ "$1" = "--macos" ]; then
+  : # https://github.com/Cleafy/sxkdvm
+else
+  docker build -f ~/dotfiles/test/Dockerfile -t dottest-all ~/dotfiles
+  docker run dottest-all "sudo ./scripts/prepare_system.sh"
+fi
