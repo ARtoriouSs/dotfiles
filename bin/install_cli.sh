7ad95b53fd790f34d07c65cd87948289c39989c6
Alexander Teslovskiy <artoriousso@gmail.com>
2022-06-22 18:59:01 +0200

    Add commits signing

--- bin/install_cli.sh
+++ bin/install_cli.sh
@@ -3,7 +3,7 @@
 # prerequirements
 sudo apt update --yes
 sudo apt upgrade --yes
-sudo apt install --yes software-properties-common apt-transport-https  libcurl4-openssl-dev apt-utils libssl-dev libreadline-dev wget curl xclip
+sudo apt install --yes software-properties-common apt-transport-https libcurl4-openssl-dev apt-utils libssl-dev libreadline-dev wget curl git xclip
 
 # python and pip
 sudo apt install --yes python2 python2.7 python3 python3-pip
