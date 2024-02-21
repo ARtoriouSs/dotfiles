7bab4449cdf264d9e12c3697108d3904d7de7b85
Alexander Teslovskiy <artoriousso@gmail.com>
2024-02-21 17:39:24 +0100

    Fix :Specl

--- vim/lua/_development.lua
+++ vim/lua/_development.lua
@@ -1,7 +1,7 @@
 -- run specs in beside tmux pane
 vim.api.nvim_create_user_command('Spec', "silent exec '!run-beside spec\\ ' . expand('%')", {bang = true})
 vim.api.nvim_create_user_command('SPec', 'Spec', {bang = true})
-vim.api.nvim_create_user_command('Specl', "silent exec '!run-beside spec\\ ' . expand('%') . ':' . line('%')", {bang = true})
+vim.api.nvim_create_user_command('Specl', "silent exec '!run-beside spec\\ ' . expand('%') . ':' . line('.')", {bang = true})
 vim.api.nvim_create_user_command('SPecl', "Specl", {bang = true})
 vim.api.nvim_create_user_command('Speca', "silent exec '!run-beside spec'", {bang = true})
 vim.api.nvim_create_user_command('SPeca', 'Speca', {bang = true})
