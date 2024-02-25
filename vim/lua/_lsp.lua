e2c0f72c1ffa805101da41c7da569b83914dc5ff
Alexander Teslovskiy <artoriousso@gmail.com>
2024-02-25 19:59:44 +0100

    Add more LSPs

--- vim/lua/_lsp.lua
+++ vim/lua/_lsp.lua
@@ -1,13 +1,30 @@
 local lsp_zero = require('lsp-zero')
 
-lsp_zero.on_attach(function(client, bufnr)
+lsp_zero.on_attach(function(_client, bufnr)
   lsp_zero.default_keymaps({buffer = bufnr}) -- :help lsp-zero-keybindings
 end)
 
 -- language server manager - :help lsp-zero-guide:integrate-with-mason-nvim
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = {'solargraph'},
+  -- https://github.com/williamboman/mason-lspconfig.nvim#available-lsp-servers
+  ensure_installed = {
+    'typos_lsp',
+    'solargraph', -- Ruby
+    'elixirls',
+    'clangd',
+    'lua_ls',
+    'vimls',
+    'bashls',
+    'sqlls',
+    'dockerls',
+    'html',
+    'cssls',
+    'jsonls',
+    'yamlls',
+    'lemminx', -- XML
+    'marksman' -- Markdown
+  },
   handlers = {
     lsp_zero.default_setup,
   }
