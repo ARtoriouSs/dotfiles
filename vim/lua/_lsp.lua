100c527b86416b6e4ece441013952677e9f46317
Alexander Ciesłowski <artoriousso@gmail.com>
2025-09-05 16:54:12 +0200

    Make gd jump to the first found ref

--- vim/lua/_lsp.lua
+++ vim/lua/_lsp.lua
@@ -51,7 +51,23 @@ vim.api.nvim_create_autocmd('LspAttach', {
     local opts = {buffer = event.buf}
 
     vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-    vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+    vim.keymap.set('n', 'gl', '<cmd>lua vim.lsp.buf.definition()<cr>', opts) -- go to definition with quickfix list
+    vim.keymap.set('n', 'gd', function() -- go to definition without quickfix list
+      local params = vim.lsp.util.make_position_params()
+      vim.lsp.buf_request(0, 'textDocument/definition', params, function(err, result, ctx, config)
+        if err then
+          vim.notify('Error getting definition: ' .. err.message, vim.log.levels.ERROR)
+          return
+        end
+        if not result or vim.tbl_isempty(result) then
+          vim.notify('No definition found', vim.log.levels.INFO)
+          return
+        end
+
+        local first_result = result[#result] or result
+        vim.lsp.util.jump_to_location(first_result, 'utf-8')
+      end)
+    end, opts)
     vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
     vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
     vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
